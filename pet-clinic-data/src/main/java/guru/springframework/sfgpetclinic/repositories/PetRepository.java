7371f78025f591c68e4371d75bd39972189bbf4f
Med <m3d13l@gmail.com>
2021-06-30 13:59:44 +0100

    created crud repositories

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.models.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+    
+}
